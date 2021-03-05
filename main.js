93acb52b6f16ff87795fed9bfba7bf8919607603
Stephanie Buijs <stephanie@fonk.amsterdam>
2021-03-05 12:06:13 +0100

    wip

--- main.js
+++ main.js
@@ -1,42 +1,46 @@
 var obj ={
-startAmount: "0"
+startAmount: "0",
+amount: "25"
 }
 var objOne = {
-    amount: "25",
+    startAmount: "0",
+    amount: "25"
 };
 
 var objTwo = {
+    startAmount: "0",
     amount: "50"
 };
 
 var objThree = {
+    startAmount: "0",
     amount: "75"
 };
 
 var count = 0;
 
 function searchDonuts(name){
-    var el = document.getElementsByClassName(name);
-    for (var i = 0, l = el.length; i < l; i++) {
+    //finding all the donut elements
+    var element = document.getElementsByClassName(name);
+    for (var i = 0, l = element.length; i < l; i++) {
         console.log('Dit is een donut');
-        
     }
 }
 
 
-function findAndUpdate() {
-	var btn = document.getElementById('addOne');
-	btn.onclick = function () {
-		console.log('click');
-		count++;
-		changeContent();
-	}
+function changeContent(name, text) {
+    if (obj.startAmount > obj.amount) return;
+    //replace the content of the element
+    var element = document.getElementsByClassName(name);
+    console.log('Het werkt');
+    element.innerText = obj.startAmount;
 }
+
+
 
 document.addEventListener('DOMContentLoaded', function (event) {
     console.log('DOM ready');
     searchDonuts('donut');
-    changeContent(obj.startAmount +"%");
-    findAndUpdate();
+    changeContent(obj.startAmount + "%");
 });
 
